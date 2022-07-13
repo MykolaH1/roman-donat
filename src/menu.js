e0cae00df29c94cb98e5c854ab6dd99fbfdb652a
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-07-13 22:45:36 +0300

    add body overflow

--- src/menu.js
+++ src/menu.js
@@ -23,6 +23,7 @@
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
+    document.body.classList.toggle('is-open');
     body.classList.toggle('no-scroll');
     mobileMenuRef.classList.toggle('fade-in');
   });
