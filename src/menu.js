4352e72315ab60029a0a8e8c58eea01c48c9b06d
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-07-13 12:36:22 +0300

    add mobile menu links

--- src/menu.js
+++ src/menu.js
@@ -14,6 +14,35 @@
     body.classList.toggle('no-scroll');
     mobileMenuRef.classList.toggle('fade-in');
   });
+
+  const mobileLinkRef = document.querySelector('[data-menu-links');
+
+  mobileLinkRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
+    mobileMenuRef.classList.toggle('fade-in');
+  });
+
+  // const menuLinks = document.querySelectorAll('.menu-link');
+  // const headerLinks = document.querySelectorAll('.header__nav-link');
+
+  // menuLinks.forEach(link => link.addEventListener('click', clickHandler));
+  // headerLinks.forEach(link => link.addEventListener('click', clickHandler));
+
+  // function clickHandler(e) {
+  //   e.preventDefault();
+  //   const href = this.getAttribute('href');
+  //   const offsetTop = document.querySelector(href).offsetTop;
+
+  //   scroll({
+  //     top: offsetTop,
+  //     behavior: 'smooth',
+  //   });
+  // }
 })();
 
 // ////констанста з меню
